d4eb2cc74209ac577e5be1c699f76eb2a71ba4f6
joaopedroxzk <159018442+joaopedroxzk@users.noreply.github.com>
2024-02-05 10:04:15 -0300

    Add files via upload

---
+++ frase.sh
@@ -0,0 +1,11 @@
+echo "vamos iniciar as atividades"
+
+sudo apt update;
+sudo apt upgrade;
+
+echo "vamos instalar o APACHE"
+
+sudo apt install mysql-server;
+sudo apt install php;
+sudo apt install phpmyadmin;
+
